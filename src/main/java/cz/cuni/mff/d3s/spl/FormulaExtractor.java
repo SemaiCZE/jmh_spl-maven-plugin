931a0a3d38375ce80196281a1cf1fabf7f1201eb
Petr Stefan <ptr.stef@gmail.com>
2016-08-09 10:03:55 +0200

    Saving formulas to META-INF is working

--- src/main/java/cz/cuni/mff/d3s/spl/FormulaExtractor.java
+++ src/main/java/cz/cuni/mff/d3s/spl/FormulaExtractor.java
@@ -15,17 +15,17 @@ import org.openjdk.jmh.runner.BenchmarkList;
 import org.openjdk.jmh.runner.BenchmarkListEntry;
 import org.openjdk.jmh.runner.format.OutputFormat;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.*;
 
 @Mojo(name = "formula_extractor", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
 public class FormulaExtractor extends AbstractMojo {
-	private String outputPath = "META-INF/SPLFormulas";
+	private String outputPath = "target/classes/META-INF/SPLFormulas";
 	private Map<String, String> formulas;
 	/**
 	 * Cache - for fully qualified class name is cached instance of Class class
@@ -45,6 +45,7 @@ public class FormulaExtractor extends AbstractMojo {
 			saveSPLFormulas();
 		} catch (Throwable e) {
 			getLog().error("Error during getting SPL formulas: " + e.getMessage());
+			e.printStackTrace();
 		}
 	}
 
@@ -52,6 +53,8 @@ public class FormulaExtractor extends AbstractMojo {
 		Set<BenchmarkListEntry> benchmarks =
 				//BenchmarkList.defaultList().getAll(new NullOutputFormat(), new LinkedList<String>());
 				BenchmarkList.fromFile("target/classes/META-INF/BenchmarkList").getAll(new NullOutputFormat(), new LinkedList<String>());
+
+		ClassLoader classLoader = getClassLoader();
 
 		for (BenchmarkListEntry benchmark : benchmarks) {
 			// lookup to cache for class of this name
@@ -62,7 +65,7 @@ public class FormulaExtractor extends AbstractMojo {
 				benchmarkClass = classInfo.getKey();
 				classFormula = classInfo.getValue();
 			} else {
-				benchmarkClass = Class.forName(benchmark.getUserClassQName());
+				benchmarkClass = Class.forName(benchmark.getUserClassQName(), true, classLoader);
 				classFormula = null;
 				if (benchmarkClass.isAnnotationPresent(SPLFormula.class)) {
 					Annotation SPLAnnotation = benchmarkClass.getAnnotation(SPLFormula.class);
@@ -74,9 +77,18 @@ public class FormulaExtractor extends AbstractMojo {
 			}
 
 			String methodName = benchmark.getUsername().substring(benchmark.getUserClassQName().length() + 1);
-			// It's ok to call just getMethod() without arguments, because JMH testing methods can't
-			// be overloaded by different argument number or types.
-			Method benchmarkMethod = benchmarkClass.getMethod(methodName);
+
+			// Can't call getMethod(), because arguments are not known. Method name is guarantied to
+			// be unique by JMH, but it won't help us here. So loop over all methods and pick the
+			// right one.
+			// TODO: check if this can be simplified
+			Method benchmarkMethod = null;
+			for (Method benchmarkClassMethod : benchmarkClass.getMethods()) {
+				if (benchmarkClassMethod.getName().equals(methodName)) {
+					benchmarkMethod = benchmarkClassMethod;
+					break;
+				}
+			}
 
 			String methodFormula = null;
 			if (benchmarkMethod.isAnnotationPresent(SPLFormula.class)) {
@@ -95,12 +107,30 @@ public class FormulaExtractor extends AbstractMojo {
 		}
 	}
 
+	private ClassLoader getClassLoader() {
+		ClassLoader currentThreadClassLoader = Thread.currentThread().getContextClassLoader();
+
+		// Add the dir with compiled classes to the classpath
+		// Chain the current thread classloader
+		URLClassLoader urlClassLoader = null;
+		try {
+			urlClassLoader = new URLClassLoader(
+					new URL[]{new File("target/classes/").toURI().toURL()},
+					currentThreadClassLoader);
+		} catch (MalformedURLException e) {
+			// not happened
+		}
+
+		return urlClassLoader;
+	}
+
 	private void saveSPLFormulas() throws FileNotFoundException, UnsupportedEncodingException {
 		PrintWriter writer = new PrintWriter(outputPath, "UTF-8");
 		for (Map.Entry<String, String> methodEntry : formulas.entrySet()) {
 			writer.write(methodEntry.getKey());
 			writer.write(":");
 			writer.write(methodEntry.getValue());
+			writer.write("\n");
 		}
 		writer.close();
 	}
